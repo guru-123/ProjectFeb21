3837a5f0da2e65677bfb7d00f814e2516b61efe4
guru-123 <guru.infotech27@gmail.com>
2020-02-21 12:24:58 +0530

    updated

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("welcome");
 		System.out.println("To");
 		System.out.println("Home");
 	}
